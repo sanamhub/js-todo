665dd6e643ce07c31fe7f8a41153d2140c603226
Sanam <sanampakuwal27@gmail.com>
2021-05-27 11:19:26 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let addToDoButton = document.getElementById("addToDo");
+let toDoContainer = document.getElementById("toDoContainer");
+let inputField = document.getElementById("inputField");
+
+addToDoButton.addEventListener("click", function () {
+  var paragraph = document.createElement("p");
+  paragraph.classList.add("paragraph-styling");
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+  inputField.value = "";
+
+  paragraph.addEventListener("click", function () {
+    paragraph.style.textDecoration = "line-through";
+  });
+
+  paragraph.addEventListener("dblclick", function () {
+    toDoContainer.removeChild(paragraph);
+  });
+});
+
+// Save on Enter
+var input = document.getElementById("inputField");
+input.addEventListener("keyup", function (e) {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    document.getElementById("addToDo").click();
+  }
+});
